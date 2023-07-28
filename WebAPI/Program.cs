ddf19a04395d49342bea3ab2eb8706448dce14df
ZaytsevGleb <gzaytsev2000@gmail.com>
2023-07-28 14:34:13 +0300

    init

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using DataAccess;
 using DataAccess.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using WebAPI;
 
 namespace WebApi
@@ -11,17 +12,36 @@ namespace WebApi
         {
             var host = CreateHostBuilder(args).Build();
 
-            using var scope = host.Services.CreateScope();
-            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await context.Database.MigrateAsync();
+            try
+            {
+                using var scope = host.Services.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
 
-            await SeedData.AddSeedDataAsync(scope.ServiceProvider);
+                await SeedData.AddSeedDataAsync(scope.ServiceProvider);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An error occurred seeding the DB.");
+                await Log.CloseAndFlushAsync();
+                throw;
+            }
 
             await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
              Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .ConfigureLogging((hostBuilder, loggingBuilder) =>
+                {
+                    var logger = new LoggerConfiguration()
+                   .ReadFrom.Configuration(hostBuilder.Configuration)
+                   .Enrich.FromLogContext()
+                   .CreateLogger();
+
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddSerilog(logger);
+                });
     }
 }
