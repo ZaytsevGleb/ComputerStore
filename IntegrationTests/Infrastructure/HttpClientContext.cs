6ec66a2031bcabcc6b9f62908fbdef654ff10b78
Mikita Antonau <mikita.a@bluetape.com>
2023-08-07 12:34:07 +0300

    HttpClientContext

---
+++ IntegrationTests/Infrastructure/HttpClientContext.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace IntegrationTests.Infrastructure;
+
+public sealed class HttpClientContext
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpClientContext(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<T> GetAsync<T>(string path)
+    {
+        var response = await _httpClient.GetAsync(path);
+        if (!response.IsSuccessStatusCode)
+            return default!;
+
+        return await ConvertResponseTtDto<T>(response);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync<T>(string path)
+    {
+        var response = await _httpClient.GetAsync(path);
+        if (!response.IsSuccessStatusCode)
+            return default!;
+
+        return await ConvertResponseTtDto<IEnumerable<T>>(response);
+    }
+
+    public async Task<T> CreateAsync<T>(string path, T dto)
+    {
+        var byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dto)));
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var response = await _httpClient.PostAsync(path, byteContent);
+        return await ConvertResponseTtDto<T>(response);
+    }
+    public async Task<T> UpdateAsync<T>(string path, Guid id, T dto)
+    {
+        var byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dto)));
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var response = await _httpClient.PutAsync($"path/{id}", byteContent);
+        return await ConvertResponseTtDto<T>(response);
+    }
+
+    public async Task DeleteAsync(string path, Guid id)
+    {
+        var response = await _httpClient.DeleteAsync($"{path}/{id}");
+        if (!response.IsSuccessStatusCode)
+            throw new Exception(response.StatusCode.ToString());
+    }
+
+    private async Task<T> ConvertResponseTtDto<T>(HttpResponseMessage response)
+    {
+        var stringContent = await response.Content.ReadAsStringAsync();
+        if (stringContent == null)
+        {
+            return default!;
+        }
+
+        var dto = JsonConvert.DeserializeObject<T>(stringContent);
+
+        return dto ?? default!;
+    }
+
+    public void Displose()
+    {
+        _httpClient.Dispose();
+    }
+}
--- IntegrationTests/ProductsTests.cs
+++ IntegrationTests/ProductsTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using IntegrationTests.Infrastructure;
+using WebAPI.Dtos;
 using Xunit;
 
 namespace IntegrationTests;
@@ -7,12 +8,17 @@ namespace IntegrationTests;
 public sealed class ProductsTests : IntegrationTest
 {
     [Fact]
+    public async Task GetProduct_ShouldReturnProduct()
+    {
+        var productDto = await HttpClient.GetAsync<ProductDto>("api/products/249c7e5c-caa7-4786-8865-7558cf439cbc");
+
+        productDto.Should().NotBeNull();
+    }
+
+    [Fact]
     public async Task GetProducts_ShouldReturnProducts()
     {
-        var response = await HttpClient.GetAsync("api/products");
-        response.EnsureSuccessStatusCode();
-
-        var responseString = await response.Content.ReadAsStringAsync();
-        responseString.Should().NotBeNull();
+        var productDtos = await HttpClient.GetAllAsync<ProductDto>("api/products");
+        productDtos.Should().NotBeNull();
     }
 }
