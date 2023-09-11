362154f4b52b8d773e13bcab32690f4e0b697043
ZaytsevGleb <gzaytsev2000@gmail.com>
2023-09-12 00:00:25 +0300

    HttpClient refactoring

--- IntegrationTests/Infrastructure/HttpClientContext.cs
+++ IntegrationTests/Infrastructure/HttpClientContext.cs
@@ -1,8 +1,4 @@
-﻿using System.Net.Http.Headers;
-using System.Text;
-using Newtonsoft.Json;
-
-namespace IntegrationTests.Infrastructure;
+﻿namespace IntegrationTests.Infrastructure;
 
 public sealed class HttpClientContext
 {
@@ -17,52 +13,33 @@ public sealed class HttpClientContext
     {
         var response = await _httpClient.GetAsync($"{path}/{id}");
 
-        return response.IsSuccessStatusCode ? await ConvertResponseToDto<T>(response) : default;
+        return response.IsSuccessStatusCode ? await response.ConvertToDto<T>() : default;
     }
 
     public async Task<IEnumerable<T>?> GetAllAsync<T>(string path, string title)
     {
         var response = await _httpClient.GetAsync($"{path}?title={title}");
 
-        return response.IsSuccessStatusCode ? await ConvertResponseToDto<IEnumerable<T>>(response) : default;
+        return response.IsSuccessStatusCode ? await response.ConvertToDto<IEnumerable<T>>() : default;
     }
 
     public async Task<T?> CreateAsync<T>(string path, T dto)
     {
-        var byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dto)));
-        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        var response = await _httpClient.PostAsync(path, dto.ConvertToRequest());
 
-        var response = await _httpClient.PostAsync(path, byteContent);
-
-        return response.IsSuccessStatusCode ? await ConvertResponseToDto<T>(response) : default;
+        return response.IsSuccessStatusCode ? await response.ConvertToDto<T>() : default;
     }
 
     public async Task<T?> UpdateAsync<T>(string path, Guid id, T dto)
     {
-        var byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dto)));
-        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        var response = await _httpClient.PutAsync($"{path}/{id}", dto.ConvertToRequest());
 
-        var response = await _httpClient.PutAsync($"{path}/{id}", byteContent);
-
-        return response.IsSuccessStatusCode ? await ConvertResponseToDto<T>(response) : default;
+        return response.IsSuccessStatusCode ? await response.ConvertToDto<T>() : default;
     }
 
     public async Task DeleteAsync(string path, Guid id)
     {
         await _httpClient.DeleteAsync($"{path}/{id}");
-    }
-
-    private async Task<T?> ConvertResponseToDto<T>(HttpResponseMessage response)
-    {
-        var stringContent = await response.Content.ReadAsStringAsync();
-        if (stringContent is null)
-        {
-            return default;
-        }
-
-        var dto = JsonConvert.DeserializeObject<T>(stringContent);
-
-        return dto ?? default;
     }
 
     public void Dispose()
---
+++ IntegrationTests/Infrastructure/HttpClientExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace IntegrationTests.Infrastructure;
+
+public static class HttpClientExtensions
+{
+    public static ByteArrayContent ConvertToRequest<T>(this T dto)
+    {
+        var byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dto)));
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        return byteContent;
+    }
+
+    public static async Task<T?> ConvertToDto<T>(this HttpResponseMessage response)
+    {
+        var stringContent = await response.Content.ReadAsStringAsync();
+        if (stringContent is null) return default;
+
+        var dto = JsonConvert.DeserializeObject<T>(stringContent);
+
+        return dto ?? default;
+    }
+}
